1980a20e521f02f7cb5246debc732d6648f9aca9
Qasim <494424+hashtaginfosec@users.noreply.github.com>
2022-01-26 23:44:22 -0500

    Updated helper messages

--- Get-ADRecon.ps1
+++ Get-ADRecon.ps1
@@ -35,11 +35,11 @@ Write-Output "[+] Listing all computers."| Tee-Object -FilePath .\domain-recon.t
 Get-ADComputer -Filter * -Properties OperatingSystem | Select-Object Name,Enabled,OperatingSystem| Tee-Object -FilePath .\domain-recon.txt -Append
 
 # Kerberoastable users.
-Write-Output "[+] Listing all kerberoastable users."| Tee-Object -FilePath .\domain-recon.txt -Append
+Write-Output "[+] Listing all kerberoastable users. Krbtgt user is expected but others should be reviewed. Any user with a service principal name set can be Kerberoasted for offline password guessing. Use managed service accounts instead."| Tee-Object -FilePath .\domain-recon.txt -Append
 Get-ADUser -Filter {ServicePrincipalName -ne "$null"} -Properties ServicePrincipalName | Select-Object name, ServicePrincipalName | Tee-Object -FilePath .\domain-recon.txt -Append
 
 # ASREP roastable users.
-Write-Output "[+] Listing all ASREP Roastable Users, if any."| Tee-Object -FilePath .\domain-recon.txt -Append
+Write-Output "[+] Listing all ASREP Roastable Users, if any. This is not normal because any user can get a TGT encrypted with ASREP Roastable user's password hash that can be put to offline brute force. "| Tee-Object -FilePath .\domain-recon.txt -Append
 Get-ADUser -Filter {DoesNotRequirePreauth -eq $True} -Properties DoesNotRequirePreAuth | Select-Object name, DoesNotRequirePreAuth| Tee-Object -FilePath .\domain-recon.txt -Append
 
 # Domain Admins.
@@ -51,19 +51,19 @@ Write-Output "[+] Listing all users with admin in their name"| Tee-Object -FileP
 Get-ADUser -Filter "Name -like 'admin*'" |Select-Object SamAccountName, enabled | Tee-Object -FilePath .\domain-recon.txt -Append
 
 # Unconstrained delegation.
-Write-Output "[+] Listing computers allowed unconstrained delegation. It is required for the DC to have unconstrained delegation. Any other sytsems showing here should be reviewed."| Tee-Object -FilePath .\domain-recon.txt -Append
+Write-Output "[+] Listing computers allowed unconstrained delegation. It is required for the DC to have unconstrained delegation. Any other sytsems showing here should be reviewed because these servers are a treasure trove. Unconstrained delegation means these servers get to keep the TGT for a user and can impersonate that user."| Tee-Object -FilePath .\domain-recon.txt -Append
 Get-ADComputer -Filter {TrustedForDelegation -eq $True} -Properties TrustedForDelegation | Select-Object name, TrustedForDelegation | Tee-Object -FilePath .\domain-recon.txt -Append
 
-Write-Output "[+] Listing all unconstrained delegation users. This is not normal and should be reviewed."| Tee-Object -FilePath .\domain-recon.txt -Append
+Write-Output "[+] Listing all unconstrained delegation users. This is not normal and should be reviewed. constrained delegation means the user or server can delegate to a sepcific service on a specific system on behalf of another user. This is often a privilege escalation method."| Tee-Object -FilePath .\domain-recon.txt -Append
 Get-ADUser -Filter {TrustedForDelegation -eq $True} -Properties TrustedForDelegation |Select-Object SamAccountName,TrustedForDelegation| Tee-Object -FilePath .\domain-recon.txt -Append
 
 # Constrained delegation.
-Write-Output "[+] Listing all computers with constrained delegation. This should be reviewed."| Tee-Object -FilePath .\domain-recon.txt -Append
+Write-Output "[+] Listing all computers with constrained delegation. This should be reviewed. Constrained delegation means the user or server can delegate to a sepcific service on a specific system on behalf of another user. This is often a privilege escalation method."| Tee-Object -FilePath .\domain-recon.txt -Append
 Get-ADComputer -Filter {msDS-AllowedToDelegateTo -ne "$null"} -Properties msDS-AllowedToDelegateTo |Select-Object DNSHostName, msDS-AllowedToDelegateTo | Tee-Object -FilePath .\domain-recon.txt -Append
 
 # GPOs
 
-Write-Output "[+] Printing permission for all GPOs."| Tee-Object -FilePath .\domain-recon.txt -Append
+Write-Output "[+] Printing permissions for all GPOs. Review for unnecessary permissions."| Tee-Object -FilePath .\domain-recon.txt -Append
 $GPOs = Get-GPO -All
 ForEach($GPO In $GPOs){
     $GPOPerms = Get-GPPermissions -DisplayName $GPO.DisplayName -All
